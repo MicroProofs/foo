9e2ba58016875f3b05ff777aec97f57c22984b26
microproofs <kwhitemsg@gmail.com>
2023-10-29 16:03:09 -0400

    finally managed to prove (force (delay m)) = m

--- Bar.lean
+++ Bar.lean
@@ -51,70 +51,123 @@ def Stack := List Frame
 deriving Repr
 
 inductive State where
-  | compute (m : Term) (ρ : List (String × Value)) (s : Stack) : State
-  | ret (v : Value) (s : Stack) : State
+  | compute (ρ : List (String × Value)) (s : Stack) : State
+  | ret (s : Stack) : State
   | error : State
-  | halt (v: Value) : State
+  | halt : State
 deriving Repr
 
+inductive Instruction where
+  | term (t: Term): Instruction
+  | value (v: Value): Instruction
 
-def lookupVar (e: List (String × Value)) (x: String) : Option Value :=
+def lookup_var (e: List (String × Value)) (x: String) : Option Value :=
   match e with
   | []  => none
-  | (y, v) :: e => if x == y then some v else lookupVar e x
+  | (y, v) :: e => if x == y then some v else lookup_var e x
 
-inductive big_step : State -> State -> Prop
-  | computeLookup {x p s} :
-    big_step (State.compute (Term.var x) p s) (match lookupVar p x with | none => State.error | some v => State.ret v s)
-  | computeConstant {c p s} : big_step (State.compute (Term.con c) p s) (State.ret (Value.vCon c) s)
-  | computeLambda {x m p s} : big_step (State.compute (Term.lam x m) p s) (State.ret (Value.vLam x m p) s)
-  | computeDelay {m p s} : big_step (State.compute (Term.delay m) p s) (State.ret (Value.vDelay m p) s)
-  | computeForce {m p s} : big_step (State.compute (Term.force m) p s) (State.compute m p (Frame.force :: s))
-  | computeApp {m n p s} : big_step (State.compute (Term.app m n) p s) (State.compute m p (Frame.leftAppTerm n p :: s))
-  | computeBuiltin {b p s} : big_step (State.compute (Term.builtin b) p s) (State.ret (Value.vBuiltin (BuiltinValue.builtin b)) s)
-  | computeError {p s} : big_step (State.compute Term.error p s) State.error
-  | ret {v s} : big_step (State.ret v s) (State.halt v)
-  | retLeftAppTerm {m p v s} : big_step (State.ret v (Frame.leftAppTerm m p :: s)) (State.compute m p (Frame.rightApp v :: s))
-  | retRightApp {x m p v s} : big_step (State.ret v (Frame.rightApp (Value.vLam x m p) :: s)) (State.compute m ((x, v) :: p) s)
-  | retLeftAppValue {x m p v s} : big_step (State.ret (Value.vLam x m p) (Frame.leftAppValue v :: s)) (State.compute m ((x, v) :: p) s)
-  | retRightAppBuiltin {b v s} : big_step (State.ret v (Frame.rightApp (Value.vBuiltin b) :: s)) (State.ret (Value.vBuiltin (BuiltinValue.app b v)) s)
-  | retLeftAppValueBuiltin {b v s} : big_step (State.ret (Value.vBuiltin b) (Frame.leftAppValue v :: s)) (State.ret (Value.vBuiltin (BuiltinValue.app b v)) s)
-  | retDelay {m p s} : big_step (State.ret (Value.vDelay m p) (Frame.force :: s)) (State.compute m p s)
-  | retBuiltin {b s} : big_step (State.ret (Value.vBuiltin b) (Frame.force :: s)) (State.ret (Value.vBuiltin (BuiltinValue.force b)) s)
-  | catchAll {s} : big_step s State.error
+-- inductive big_step : State -> State -> Prop
+--   | computeLookup {x p s} :
+--     big_step (State.compute (Term.var x) p s) (match lookup_var p x with | none => State.error | some v => State.ret v s)
+--   | computeConstant {c p s} : big_step (State.compute (Term.con c) p s) (State.ret (Value.vCon c) s)
+--   | computeLambda {x m p s} : big_step (State.compute (Term.lam x m) p s) (State.ret (Value.vLam x m p) s)
+--   | computeDelay {m p s} : big_step (State.compute (Term.delay m) p s) (State.ret (Value.vDelay m p) s)
+--   | computeForce {m p s} : big_step (State.compute (Term.force m) p s) (State.compute m p (Frame.force :: s))
+--   | computeApp {m n p s} : big_step (State.compute (Term.app m n) p s) (State.compute m p (Frame.leftAppTerm n p :: s))
+--   | computeBuiltin {b p s} : big_step (State.compute (Term.builtin b) p s) (State.ret (Value.vBuiltin (BuiltinValue.builtin b)) s)
+--   | computeError {p s} : big_step (State.compute Term.error p s) State.error
+--   | ret {v s} : big_step (State.ret v s) (State.halt v)
+--   | retLeftAppTerm {m p v s} : big_step (State.ret v (Frame.leftAppTerm m p :: s)) (State.compute m p (Frame.rightApp v :: s))
+--   | retRightApp {x m p v s} : big_step (State.ret v (Frame.rightApp (Value.vLam x m p) :: s)) (State.compute m ((x, v) :: p) s)
+--   | retLeftAppValue {x m p v s} : big_step (State.ret (Value.vLam x m p) (Frame.leftAppValue v :: s)) (State.compute m ((x, v) :: p) s)
+--   | retRightAppBuiltin {b v s} : big_step (State.ret v (Frame.rightApp (Value.vBuiltin b) :: s)) (State.ret (Value.vBuiltin (BuiltinValue.app b v)) s)
+--   | retLeftAppValueBuiltin {b v s} : big_step (State.ret (Value.vBuiltin b) (Frame.leftAppValue v :: s)) (State.ret (Value.vBuiltin (BuiltinValue.app b v)) s)
+--   | retDelay {m p s} : big_step (State.ret (Value.vDelay m p) (Frame.force :: s)) (State.compute m p s)
+--   | retBuiltin {b s} : big_step (State.ret (Value.vBuiltin b) (Frame.force :: s)) (State.ret (Value.vBuiltin (BuiltinValue.force b)) s)
+--   | catchAll {s} : big_step s State.error
 
+  inductive small_step : (Instruction × State) -> (Instruction × State) -> Prop
+  | seq {m m' m'' s s' s''} (h: small_step (m, s) (m', s')) (h': small_step (m', s') (m'', s'')):
+    small_step (m, s) (m'', s'')
 
+  | computeLookup {x p s} : small_step
+    ((Instruction.term (Term.var x)), (State.compute p s))
+    (match lookup_var p x with | none => ((Instruction.term Term.error), State.error) | some v => ((Instruction.value v), State.ret s))
 
-
-partial def reduc (st : State) : State :=
-  match st with
-  | State.compute (Term.var x) ρ s => match lookupVar ρ x with
-    | none => State.error
-    | some v => reduc (State.ret v s)
-  | State.compute (Term.con c) _ s => reduc (State.ret (Value.vCon c) s)
-  | State.compute (Term.lam x m) ρ s => reduc (State.ret (Value.vLam x m ρ) s)
-  | State.compute (Term.delay m) ρ s => reduc (State.ret (Value.vDelay m ρ) s)
-  | State.compute (Term.force m) ρ s => reduc (State.compute m ρ (Frame.force :: s))
-  | State.compute (Term.app m n) p s => reduc (State.compute m p (Frame.leftAppTerm n p :: s))
-  | State.compute (Term.builtin b) _ s => reduc (State.ret (Value.vBuiltin (BuiltinValue.builtin b)) s)
-  | State.compute Term.error _ _ => State.error
-  | State.ret v [] => State.halt v
-  | State.ret v (Frame.leftAppTerm m p :: s) => reduc (State.compute m p (Frame.rightApp v :: s))
-  | State.ret v (Frame.rightApp (Value.vLam x m p) :: s) => reduc (State.compute m ((x, v) :: p) s)
-  | State.ret (Value.vLam x m p) (Frame.leftAppValue v :: s) => reduc (State.compute m ((x, v) :: p) s)
-  | State.ret v (Frame.rightApp (Value.vBuiltin b) :: s) => reduc (State.ret (Value.vBuiltin (BuiltinValue.app b v)) s)
-  | State.ret (Value.vBuiltin b) (Frame.leftAppValue v :: s) => reduc (State.ret (Value.vBuiltin (BuiltinValue.app b v)) s)
+  | computeConstant {c p s} : small_step (Instruction.term (Term.con c) ,State.compute p s) (Instruction.value (Value.vCon c), State.ret s)
+  | computeLambda {x m p s} : small_step (Instruction.term (Term.lam x m), State.compute p s) (Instruction.value (Value.vLam x m p), State.ret s)
+  | computeDelay {m p s} : small_step (Instruction.term (Term.delay m), State.compute p s) (Instruction.value (Value.vDelay m p), State.ret s)
+  | computeForce {m p s} : small_step (Instruction.term (Term.force m), State.compute p s) (Instruction.term m, State.compute p (Frame.force :: s))
+  | computeApp {m n p s} : small_step (Instruction.term (Term.app m n), State.compute p s) (Instruction.term m, State.compute p (Frame.leftAppTerm n p :: s))
+  | computeBuiltin {b p s} : small_step (Instruction.term (Term.builtin b), State.compute  p s) (Instruction.value (Value.vBuiltin (BuiltinValue.builtin b)), State.ret s)
+  | computeError {p s} : small_step (Instruction.term Term.error, State.compute p s) (Instruction.term Term.error, State.error)
+  | ret {v} : small_step (Instruction.value v, State.ret []) (Instruction.value v, State.halt)
+  | retLeftAppTerm {m p v s} : small_step (Instruction.value v, State.ret (Frame.leftAppTerm m p :: s)) (Instruction.term m, State.compute p (Frame.rightApp v :: s))
+  | retRightApp {x m p v s} : small_step (Instruction.value v, State.ret (Frame.rightApp (Value.vLam x m p) :: s)) (Instruction.term m, State.compute ((x, v) :: p) s)
+  | retLeftAppValue {x m p v s} : small_step (Instruction.value (Value.vLam x m p), State.ret (Frame.leftAppValue v :: s)) (Instruction.term m, State.compute ((x, v) :: p) s)
+  | retRightAppBuiltin {b v s} : small_step (Instruction.value v, State.ret (Frame.rightApp (Value.vBuiltin b) :: s)) (Instruction.value (Value.vBuiltin (BuiltinValue.app b v)), State.ret s)
+  | retLeftAppValueBuiltin {b v s} : small_step (Instruction.value (Value.vBuiltin b), State.ret  (Frame.leftAppValue v :: s)) (Instruction.value (Value.vBuiltin (BuiltinValue.app b v)), State.ret s)
   -- TODO: Eval builtins
-  | State.ret (Value.vDelay m p) (Frame.force :: s) => reduc (State.compute m p s)
-  | State.ret (Value.vBuiltin b) (Frame.force :: s) => reduc (State.ret (Value.vBuiltin (BuiltinValue.force b)) s)
-  | _ => State.error
-
-def run (p: Program) : State :=
-  reduc (State.compute p.m [] [])
-
-def test2 : State :=
-  let x := run ({v := 0, m := Term.app (Term.lam "so" (Term.var "so")) (Term.con 5)})
-  x
+  | retDelay {m p s} : small_step (Instruction.value (Value.vDelay m p), State.ret (Frame.force :: s)) (Instruction.term m, State.compute p s)
+  | retBuiltin {b s} : small_step (Instruction.value (Value.vBuiltin b), State.ret (Frame.force :: s)) (Instruction.value (Value.vBuiltin (BuiltinValue.force b)), State.ret s)
+  | catchAll {m s} : small_step (m, s) ((Instruction.term Term.error), State.error)
 
 
-#eval test2
+infix: 90 "⟶" => small_step
+
+
+-- def simpleTerm: Program :=
+--   ({v := 0, m := Term.app (Term.lam "so" (Term.var "so")) (Term.con 5)})
+
+
+theorem small_step_compute_lambda
+{x m p s} : (Instruction.term (Term.lam x m), State.compute  p s) ⟶ (Instruction.value (Value.vLam x m p), State.ret s) := by
+  apply small_step.computeLambda
+
+
+theorem force_delay_just_term
+  {m p s}:
+  (Instruction.term (Term.force (Term.delay m)), State.compute p s) ⟶ (Instruction.value (Value.vDelay m p), State.ret (Frame.force :: s)) := by
+    have h: (Instruction.term (Term.force (Term.delay m)), State.compute p s) ⟶ (Instruction.term (Term.delay m), State.compute p (Frame.force :: s)) := by
+      apply small_step.computeForce
+    have h': (Instruction.term (Term.delay m), State.compute p (Frame.force :: s)) ⟶ (Instruction.value (Value.vDelay m p), State.ret (Frame.force :: s)) := by
+      apply small_step.computeDelay
+    exact small_step.seq h h'
+
+
+
+
+
+
+-- partial def reduc (st : State) : State :=
+--   match st with
+--   | State.compute (Term.var x) ρ s => match lookup_var ρ x with
+--     | none => State.error
+--     | some v => reduc (State.ret v s)
+--   | State.compute (Term.con c) _ s => reduc (State.ret (Value.vCon c) s)
+--   | State.compute (Term.lam x m) ρ s => reduc (State.ret (Value.vLam x m ρ) s)
+--   | State.compute (Term.delay m) ρ s => reduc (State.ret (Value.vDelay m ρ) s)
+--   | State.compute (Term.force m) ρ s => reduc (State.compute m ρ (Frame.force :: s))
+--   | State.compute (Term.app m n) p s => reduc (State.compute m p (Frame.leftAppTerm n p :: s))
+--   | State.compute (Term.builtin b) _ s => reduc (State.ret (Value.vBuiltin (BuiltinValue.builtin b)) s)
+--   | State.compute Term.error _ _ => State.error
+--   | State.ret v [] => State.halt v
+--   | State.ret v (Frame.leftAppTerm m p :: s) => reduc (State.compute m p (Frame.rightApp v :: s))
+--   | State.ret v (Frame.rightApp (Value.vLam x m p) :: s) => reduc (State.compute m ((x, v) :: p) s)
+--   | State.ret (Value.vLam x m p) (Frame.leftAppValue v :: s) => reduc (State.compute m ((x, v) :: p) s)
+--   | State.ret v (Frame.rightApp (Value.vBuiltin b) :: s) => reduc (State.ret (Value.vBuiltin (BuiltinValue.app b v)) s)
+--   | State.ret (Value.vBuiltin b) (Frame.leftAppValue v :: s) => reduc (State.ret (Value.vBuiltin (BuiltinValue.app b v)) s)
+--   -- TODO: Eval builtins
+--   | State.ret (Value.vDelay m p) (Frame.force :: s) => reduc (State.compute m p s)
+--   | State.ret (Value.vBuiltin b) (Frame.force :: s) => reduc (State.ret (Value.vBuiltin (BuiltinValue.force b)) s)
+--   | _ => State.error
+
+-- def run (p: Program) : State :=
+--   reduc (State.compute p.m [] [])
+
+-- def test2 : State :=
+--   let x := run ({v := 0, m := Term.app (Term.lam "so" (Term.var "so")) (Term.con 5)})
+--   x
+
+
+-- #eval test2
